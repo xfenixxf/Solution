0bc131feabf5baf2bda3901ab23a953d4346d9bb
xfenixxf <x.fenix.x.f@gmail.com>
2026-05-12 21:35:53 +0300

    Добавьте файлы проекта.

---
+++ DataType1/Program1.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DateType1
+{
+    internal class Program1
+    {
+        /// <summary>
+        /// функция для расчёта сложных процентов по годам
+        /// </summary>
+        /// <param name="initial_deposit"> Начальный депозит</param>
+        /// <param name="years">года</param>
+        /// <param name="interest_rate">процент ставки</param>
+        /// <returns></returns>
+        public static string CalculatePercent(double initial_deposit, int years, int interest_rate)
+        {
+            string resultString = "";
+            for (int i = 0; i < years; i++)
+            {
+                initial_deposit = initial_deposit + ((initial_deposit * interest_rate) / 100);
+                resultString = resultString + $"Год{i + 1}: {initial_deposit:F2} руб.\n";
+
+            }
+            return resultString;
+        }
+
+        static void Main(string[] args)
+        {
+            double initial_deposit = 0;
+            int years = 0;
+            int interest_rate = 0;
+            Console.WriteLine($"начальный вклад");
+            initial_deposit = Convert.ToDouble(Console.ReadLine());
+            if (initial_deposit < 0) { Console.WriteLine($"неверное число"); return; }
+            Console.WriteLine($"количество лет");
+            years = Convert.ToInt32(Console.ReadLine());
+            if (years < 0) { Console.WriteLine($"неверное число"); return; }
+            Console.WriteLine($"годовая процентная ставка");
+            interest_rate = Convert.ToInt32(Console.ReadLine());
+            if (interest_rate < 0) { Console.WriteLine($"неверное число"); return; }
+            Console.WriteLine(CalculatePercent(initial_deposit, years, interest_rate));
+
+        }
+    }
+}
---
+++ DataType2/Program2.cs
@@ -0,0 +1,42 @@
+﻿namespace DataType2
+{
+    internal class Program2
+    {
+        public static void PrintDiamond(int x)
+        {
+            if (x % 2 == 0)
+            {
+                Console.WriteLine("нечётное число");
+                return;
+            }
+
+            int center = x / 2;
+
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < x; j++)
+                {
+                    if (Math.Abs(i - center) + Math.Abs(j - center) == center)
+                    {
+                        Console.Write("X");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+        static void Main(string[] args)
+        {
+            int x = 0;
+            Console.WriteLine($"X =");
+            x = Convert.ToInt32(Console.ReadLine());
+            if (x < 0) { Console.WriteLine($"неверное число"); return; }
+            PrintDiamond(x);
+        }
+    }
+}
+
+
